3e51a597bde2416385449d1ac4a66e83352bb5d4
szouga <szouga9@gmail.com>
2026-06-27 17:34:55 -0500

    update search functionality using api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 //alert("hello world");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  //console.log(response.data.temperature.current);
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  //make api call and update the interface
+  let apiKey = "7e1fbo71a48539tbb0610fa3a35820ef";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   //console.log(searchInput.value);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-  //call the API
-
-  //search for the city
+  searchCity(searchInput.value);
 }
+//let url = "https://jsonplaceholder.typicode.com/users/1";
+//axios.get(url).then(searchCity);
 let searchFormElement = document.querySelector("#search-form");
 //console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Paris");
